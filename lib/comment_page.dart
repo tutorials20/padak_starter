b54948f72c0abe30dbb3557154b7509a0de3c2d2
Sungshik Kim <kimss@rsupport.com>
2020-07-23 22:53:35 +0900

    ~ 206

--- lib/comment_page.dart
+++ lib/comment_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:padak_starter/model/widget/star_rating_bar.dart';
 
 class CommentPage extends StatefulWidget {
   final String movieTitle;
@@ -17,56 +18,112 @@ class CommentPageState extends State<CommentPage> {
   final scaffoldState = GlobalKey<ScaffoldState>();
   int _rating = 0;
   String _writer = "";
-  String _contents = "";
+  String _content = "";
 
-  CommentPageState(String movieTitle, String movieId){
+  CommentPageState(String movieTitle, String movieId) {
     this.movieTitle = movieTitle;
     this.movieId = movieId;
   }
 
   // 3-1. 댓글 입력 화면 (화면 구현)
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
+        key: scaffoldState,
         appBar: AppBar(
           title: Text('한줄평 작성'),
+          actions: <Widget>[_submitButton()],
         ),
-        body: Center(child: Text("Comment Page"))
-    );
-  }
+        body: WillPopScope(
+          child: SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _buildMovieTitle(),
+                    _buildUserRating(),
+                    _buildHorizontalDivider(),
+                    _buildNickNameInputForm(),
+                    _buildCommentInputForm()
+                  ],
+                )),
+          ),
+          onWillPop: () {
+            Navigator.of(context).pop(false);
+            return Future.value(false);
+          },
+        ),
+      );
 
-  Widget _buildSubmitButton(){
-    // 3-2. 댓글 입력 화면 (_buildSubmitButton)
-    return Center(child: Text("전송"));
-  }
+  // 3-2. 댓글 입력 화면 (_buildSubmitButton)
+  Widget _submitButton() => IconButton(
+        icon: Icon(Icons.send, color: Colors.white, size: 25),
+        onPressed: () {
+          if (_writer.isEmpty || _content.isEmpty) {
+            _showSnackBar('모든 정보를 입력해주세요.');
+          } else {
+            Navigator.of(context).pop(true);
+          }
+        },
+      );
 
-  Widget _buildMovieTitle(){
-    // 3-3. 댓글 입력 화면 (_buildMovieTitle)
-    return Text("영화 제목");
-  }
+  Widget _buildMovieTitle() => Container(
+      padding: EdgeInsets.all(10),
+      child: Text(widget.movieTitle,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)));
 
-  Widget _buildUserRating(){
-    // 3-4. 댓글 입력 화면 (_buildUserRating)
-    return Text("유저가 별점을 설정할 수 있는 화면");
-  }
+  Widget _buildUserRating() => Column(
+        children: <Widget>[
+          StarRatingBar(
+            onRatingChanged: (rating) {
+              setState(() {
+                _rating = rating;
+              });
+            },
+          ),
+          Text((_rating / 2.0).toString())
+        ],
+      );
 
-  Widget _buildHorizontalDivider(){
-    // 3-5. 댓글 입력 화면 (_buildHorizontalDivider)
-    return Text("회색 구분선 화면");
-  }
+  Widget _buildHorizontalDivider() => Container(
+        margin: EdgeInsets.symmetric(vertical: 14, horizontal: 4),
+        width: double.infinity,
+        height: 10,
+        color: Colors.grey.shade400,
+      );
 
-  Widget _buildNickNameInputForm(){
-    // 3-6. 댓글 입력 화면 (_buildNickNameInputForm)
-    return Text("닉네임 입력");
-  }
+  Widget _buildNickNameInputForm() => Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: TextField(
+          onChanged: (text) => _writer = text,
+          maxLines: 1,
+          maxLength: 20,
+          decoration: InputDecoration(
+              hintText: '닉네임을 입력해주세요.',
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(),
+              )),
+        ),
+      );
 
-  Widget _buildCommentInputForm(){
-    // 3-7. 댓글 입력 화면 (_buildCommentInputForm)
-    return Text("한줄평 입력");
-  }
+  Widget _buildCommentInputForm() => Container(
+        margin: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 10),
+        child: TextField(
+          onChanged: (text) => _content = text,
+          maxLines: null,
+          maxLength: 100,
+          decoration: InputDecoration(
+              hintText: '한줄평을 작성해주세요',
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                  borderSide: BorderSide(),
+                  borderRadius: BorderRadius.circular(8))),
+        ),
+      );
 
   void _showSnackBar(String text) {
-    final snackBar = SnackBar(content: Text(text));
-    scaffoldState.currentState.showSnackBar(snackBar);
+    scaffoldState.currentState.showSnackBar(SnackBar(content: Text(text)));
   }
 }
